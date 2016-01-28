144bb5352ed39a320de24293cf6346d18dd34da6
da99 <i-hate-spam-1234567@mailinator.com>
2016-01-27 21:59:15 -0600

    Added: more customizations

--- files/custom.da99.el
+++ files/custom.da99.el
@@ -1,5 +1,7 @@
 (global-linum-mode)
 (setq default-tab-width 2)
 (load-file "/apps/emacs_setup/files/multi-term.el")
+(load-file "/apps/emacs_setup/files/whitespace.el")
 (setq multi-term-program "/bin/fish")
 (define-key evil-motion-state-map (kbd "C-i") 'evil-jump-forward)
+(spacemacs/toggle-truncate-lines-off)
